2c09279f8f77ce5f21682f74461fea4a953c2210
eduardo.pocas@infosistema.com <eduardo.pocas@infosistema.com>
2024-05-10 10:04:23 +0100

    Added I am Alive feature to root URL

---
+++ sources/Runners/Joyn.DokRouterServer/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Joyn.DokRouter.Common.Payloads;
+using Joyn.DokRouter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Joyn.DokRouterServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return Content($"I am alive: {DateTime.UtcNow.ToString("u")}");
+        }
+
+        [HttpGet("/")]
+        public IActionResult Root()
+        {
+            return RedirectPermanent("/Home/Index");
+        }
+    }
+}
